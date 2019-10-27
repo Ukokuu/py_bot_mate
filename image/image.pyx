a67aaf2cae6b5dee83d71695b482e95dcd068958
ukokuu <We011242@163.com>
2019-10-27 15:22:32 -0700

    More cython tweak, seperate the find pixel method to return an array of coords and a method to color them

--- image/image.pyx
+++ image/image.pyx
@@ -1,21 +1,37 @@
 import cython
+import numpy as np
 from py_bot_mate.color.finder_c import color_same_cts2
 
+
 @cython.boundscheck(False)
-cpdef unsigned char [:, :, :] find_pixels_with_color_tolerance(unsigned char [:, :, :] image, cts):
-# set the variable extension types
- cdef int x, y, w, h
- # grab the image dimensions
- h = len(image)
- w = len(image[0])
- index = 0
+cpdef unsigned char[:, :, :] mark_pixels_with_color(unsigned char [:, :, :] image, unsigned int [:, :] coords, unsigned char[:] color):
+    length = len(coords)
+    for i in range(0, length):
+        coord = coords[i]
+        image[coord[1], coord[0], 0] = color[0]
+        image[coord[1], coord[0], 1] = color[1]
+        image[coord[1], coord[0], 2] = color[2]
 
- # loop over the image
- for x in range(0, h):
-    for y in range(0, w):
-        if color_same_cts2(cts, image[x, y]):
-            image[x,y,2] = 255
-            image[x,y,1] = 0
-            image[x,y,0] = 0
+    return image
 
- return image
+
+@cython.boundscheck(False)
+cpdef unsigned int [:, :] find_pixels_with_color_tolerance(unsigned char [:, :, :] image, cts):
+    # set the variable extension types
+    cdef int x, y, w, h
+    # grab the image dimensions
+    h = len(image)
+    w = len(image[0])
+    index = 0
+    points = np.zeros(w * h, dtype=(np.uint32,2))
+
+    # loop over the image
+    for x in range(0, h):
+        for y in range(0, w):
+            if color_same_cts2(cts, image[x, y]):
+                points[index, 0] = y
+                points[index, 1] = x
+                index += 1
+
+    points = points[~np.all(points == 0, axis=1)]
+    return points
--- test/cython/cython_test.py
+++ test/cython/cython_test.py
@@ -4,6 +4,7 @@ import pyximport
 pyximport.install()
 
 from py_bot_mate.image.image import find_pixels_with_color_tolerance
+from py_bot_mate.image.image import mark_pixels_with_color
 from py_bot_mate.color.color import Color
 from py_bot_mate.color.finder_c import color_same_cts2
 from py_bot_mate.color.color_tolerance_setting import ColorToleranceSetting
@@ -13,10 +14,13 @@ import time
 img = cv2.imread('data/dl1.png')
 cts = ColorToleranceSetting(Color.from_bgr(15857887), 6, 1.02, 6.30)
 start = time.time()
-img = np.array(find_pixels_with_color_tolerance(img, cts))
-print(img[0])
+points = find_pixels_with_color_tolerance(img, cts)
 end = time.time()
-cv2.imshow('display', img)
 print(end - start)
+start = time.time()
+img = np.array(mark_pixels_with_color(img, points, np.array([0, 0, 255], dtype='B')))
+end = time.time()
+print(end - start)
+cv2.imshow('display', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
