d68c2947cc75bbb44a82adf9e95f95e744cf6649
ukokuu <We011242@163.com>
2019-10-27 19:26:38 -0700

    Add cluster find

--- image/image.pyx
+++ image/image.pyx
@@ -1,10 +1,11 @@
 import cython
 import numpy as np
 from py_bot_mate.color.finder_c import color_same_cts2
-
+from sklearn.cluster import DBSCAN
 
 @cython.boundscheck(False)
 cpdef unsigned char[:, :, :] mark_pixels_with_color(unsigned char [:, :, :] image, unsigned int [:, :] coords, unsigned char[:] color):
+    cdef int i, length
     length = len(coords)
     for i in range(0, length):
         coord = coords[i]
@@ -17,7 +18,7 @@ cpdef unsigned char[:, :, :] mark_pixels_with_color(unsigned char [:, :, :] imag
 @cython.boundscheck(False)
 cpdef unsigned int [:, :] find_pixels_with_color_tolerance(unsigned char [:, :, :] image, cts):
     # set the variable extension types
-    cdef int x, y, w, h
+    cdef int x, y, w, h, index
     # grab the image dimensions
     h = len(image)
     w = len(image[0])
@@ -34,3 +35,21 @@ cpdef unsigned int [:, :] find_pixels_with_color_tolerance(unsigned char [:, :, 
 
     points = points[~np.all(points == 0, axis=1)]
     return points
+
+@cython.boundscheck(False)
+cpdef list find_cluster_with_color_tolerance(unsigned char [:, :, :] image, cts, min_pixels=5, eps=0.5, min_samples=5):
+    cdef int n_cluster, x, i
+    points =  find_pixels_with_color_tolerance(image, cts)
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(points)
+    labels = db.labels_
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    clusters = []
+    for x in range(0, n_clusters - 1):
+            indexs = np.array(np.where(labels == x)).flatten()
+            print(min_pixels)
+            if indexs.size < min_pixels:
+                continue
+            else:
+                clusters_points = np.array([points[i] for i in indexs])
+                clusters.append(clusters_points)
+    return clusters
--- test/cython/cython_test.py
+++ test/cython/cython_test.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.cluster import DBSCAN
 import cv2
 import time
 from random import randrange
@@ -7,30 +6,26 @@ import pyximport
 
 pyximport.install()
 
-from py_bot_mate.image.image import find_pixels_with_color_tolerance
+from py_bot_mate.image.image import find_cluster_with_color_tolerance
 from py_bot_mate.image.image import mark_pixels_with_color
 from py_bot_mate.color.color import Color
 from py_bot_mate.color.color_tolerance_setting import ColorToleranceSetting
 
 img = cv2.imread('data/dl1.png')
 cts = ColorToleranceSetting(Color.from_bgr(15857887), 6, 1.02, 6.30)
+
 start = time.time()
-points = find_pixels_with_color_tolerance(img, cts)
+clusters = find_cluster_with_color_tolerance(img, cts, min_pixels=300, eps=12, min_samples=20)
 end = time.time()
 print(end - start)
-start = time.time()
-# img = np.array(mark_pixels_with_color(img, points, np.array([0, 0, 255], dtype='B')))
-end = time.time()
-print(end - start)
-db = DBSCAN(eps=10, min_samples=30).fit(points)
-labels = db.labels_
-# Number of clusters in labels, ignoring noise if present.
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-n_noise_ = list(labels).count(-1)
-print('Estimated number of clusters: %d' % n_clusters_)
-for x in range(0, n_clusters_ - 1):
-    indexs = np.array(np.where(labels == x)).flatten()
-    img = np.array(mark_pixels_with_color(img, np.array([points[i] for i in indexs]), np.array([randrange(255), randrange(255), randrange(255)], dtype='B')))
+
+for cluster in clusters:
+    img = np.array(mark_pixels_with_color(img, np.array(cluster), np.array([randrange(255), randrange(255), randrange(
+        255)], dtype='B')))
+
 cv2.imshow('display', img)
+cv2.waitKey(0)
+cv2.waitKey(0)
+cv2.waitKey(0)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
